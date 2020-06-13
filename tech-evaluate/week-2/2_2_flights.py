92bce5aaa83087aa9f66a24dfae3b97ceed8be80
Carl Desautels <carl.desautels@grandparade.co.uk>
2020-06-13 17:27:45 +0200

    Week 2 assignment 2 adjacency list graph class

--- tech-evaluate/week-2/2_2_flights.py
+++ tech-evaluate/week-2/2_2_flights.py
@@ -1,23 +1,41 @@
 import sys
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Edge:
+    src: str
+    dest: str
+    weight: int
+
+
+@dataclass
+class Graph:
+    edges: dict = field(default_factory=dict)
+
+    def add_edge(self, src: str, dst: str, weight: int):
+        self.edges[src] = Edge(src, dst, int(weight))
 
 
 def find_connections():
-  # write your code here
+    # write your code here
+    return []
 
 
 def main():
-    flights = []
+    network = Graph()
 
     number_of_flights = int(sys.stdin.readline())
-    for i in range(number_of_flights):
-      # write your code to read the flights information
-
+    for _ in range(number_of_flights):
+        network.add_edge(*sys.stdin.readline().strip().split(" "))
+    print(network)
     limit = int(sys.stdin.readline())
     source_airport, destination_airport = sys.stdin.readline().strip().split(' ')
 
     connections = find_connections()
     for connection in connections:
-      # print connections
+        # print connections
+        print()
 
 
 if __name__ == '__main__':
