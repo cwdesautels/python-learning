b1c97d8478669c71f1b432e9d2e03ee43ab4f548
Carl Desautels <carl.desautels@grandparade.co.uk>
2020-06-13 23:24:03 +0200

    Week 2 assignment 2 Do some sorting as per spec, 2 tests still fail

--- tech-evaluate/week-2/2_2_flights.py
+++ tech-evaluate/week-2/2_2_flights.py
@@ -1,4 +1,5 @@
 import sys
+import functools
 from dataclasses import dataclass, field
 
 
@@ -10,6 +11,22 @@ class Graph:
         if src not in self.edges:
             self.edges[src] = dict()
         self.edges[src][dst] = int(weight)
+
+
+def path_comparator(a: list, b: list) -> int:
+    result = a[-1] - b[-1]
+
+    if result == 0:
+        result = len(a) - len(b)
+        if result == 0:
+            for char1, char2 in zip(" ".join(a[:-1]), " ".join(b[:-1])):
+                result = ord(char1) - ord(char2) if \
+                    'A' <= char1 <= 'Z' else \
+                    ord(char2) - ord(char1)
+                if result != 0:
+                    break
+
+    return result
 
 
 def bfs(src: str, dst: str, network: Graph):
@@ -32,14 +49,17 @@ def bfs(src: str, dst: str, network: Graph):
 
 def find_connections(src: str, dst: str, limit: int, network: Graph) -> list:
     paths = bfs(src, dst, network)
-    for path in paths:
-        cost = 0
-        i = 0
-        for i in range(len(path) - 1):
-            cost += network.edges[path[i]][path[i+1]]
-        path.append(cost)
-    paths.sort(key=lambda path: path[-1])
-    return paths
+    if paths:
+        for path in paths:
+            cost = 0
+            i = 0
+            for i in range(len(path) - 1):
+                cost += network.edges[path[i]][path[i+1]]
+            path.append(cost)
+        paths.sort(key=functools.cmp_to_key(path_comparator))
+        return paths[:limit]
+    else:
+        return [["<no solution>"]]
 
 
 def main():
@@ -49,7 +69,7 @@ def main():
     for _ in range(number_of_flights):
         network.add_edge(*sys.stdin.readline().strip().split(" "))
 
-    print(network)
+    # print(network)
     limit = int(sys.stdin.readline())
     src, dst = sys.stdin.readline().strip().split(" ")
     connections = find_connections(src, dst, limit, network)
