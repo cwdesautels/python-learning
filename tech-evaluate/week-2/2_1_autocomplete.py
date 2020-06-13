50442a727de43c5b339d645955d872ab439923fc
Carl Desautels <carl.desautels@grandparade.co.uk>
2020-06-13 15:45:36 +0200

    Week 2 assignment 1 scan dictionary per query

--- tech-evaluate/week-2/2_1_autocomplete.py
+++ tech-evaluate/week-2/2_1_autocomplete.py
@@ -1,19 +1,36 @@
+def fuzzy_match(a: str, b: str) -> bool:
+    return True
+
+
 def solve(dictionary, queries):
-    # Write your code here ...
-    pass
+    for query in queries:
+        query_length = len(query)
+        matches = []
+        for word in dictionary:
+            word_length = len(word)
+            if word_length == query_length and fuzzy_match(query, word):
+                matches.append(word)
+            elif word_length > query_length and fuzzy_match(query, word[0:query_length]):
+                matches.append(word)
+        if len(matches) == 0:
+            print("<no matches>")
+        else:
+            print(*matches)
+
 
 def main():
-  dict_size = int(input())
-  dictionary = []
-  for _ in range(dict_size):
-    dict_word = input()
-    dictionary.append(dict_word)
-  query_size = int(input())
-  queries = []
-  for _ in range(query_size):
-    query = input()
-    queries.append(query)
-  solve(dictionary, queries)
-  
+    dict_size = int(input())
+    dictionary = []
+    for _ in range(dict_size):
+        dict_word = input()
+        dictionary.append(dict_word)
+    query_size = int(input())
+    queries = []
+    for _ in range(query_size):
+        query = input()
+        queries.append(query)
+    solve(dictionary, queries)
+
+
 if __name__ == "__main__":
     main()
